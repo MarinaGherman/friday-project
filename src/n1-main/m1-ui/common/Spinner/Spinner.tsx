c017d7fdfcc3a7708a0e76c942c8afcfef41c9a5
marina <marina.gherman@icloud.com>
2022-05-25 15:22:46 +0200

    add spinner ui component

---
+++ src/n1-main/m1-ui/common/Spinner/Spinner.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import img from "../images/Spinner.svg"
+
+const Spinner = () => {
+    return (
+        <div>
+            <img src={img} alt=""/>
+        </div>
+    );
+};
+
+export default Spinner;
---
+++ src/n1-main/m1-ui/common/Spinner/SpinnerContainer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Spinner from "./Spinner";
+
+const SpinnerContainer = () => {
+    return (
+        <div>
+            <Spinner/>
+        </div>
+    );
+};
+
+export default SpinnerContainer;
